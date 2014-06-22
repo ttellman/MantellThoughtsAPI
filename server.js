554de42b7ebc8b3557f514f3e13d1376cfb89e16
ttellman <ttellmandesign@gmail.com>
2014-06-21 22:12:45 -0500

    confirmed api is working

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-var express = require('express');
-var app = express();
+//Base Setup
+
+//calll needed packages here
+var express = require('express'); //calls express
+var app = express(); //define applcation using express
+var bodyParser = require('body-parser');
 
 
-app.get('/', function(req, res){
-    res.send('Hello World!');
-})
+// configure app to use bodyParser() to get information from POST
+app.use(bodyParser());
 
-app.listen(3000);
-console.log("Here we go server started on port 3000 hold on tight");
+var port = process.env.PORT || 3000;  //set port
+
+
+//Routes for API
+var router = express.Router(); // get and instance of express Router
+
+// testing route to make sure it is working
+router.get('/', function(req, res){
+    res.json({message: 'Hooray welcome to my api'});
+});
+// add more routes to API here
+
+// Register the routes
+app.use('/api', router);
+
+//Start the Server
+app.listen(port);
+console.log('Here we go server started on '+ port +  ' hold on tight');
